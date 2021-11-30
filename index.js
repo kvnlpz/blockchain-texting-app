b6d5d057eb927e023de153487563bace6e84e1b5
rpura951 <rpura951@gmail.com>
2021-11-30 13:57:35 -0800

    Added basic login and registration

--- index.js
+++ index.js
@@ -1,48 +1,87 @@
-
-import mongoose from 'mongoose';
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const expressSession = require('express-session')({
+    secret: 'insert secret here',
+    resave: false,
+    saveUninitialized: false
+})
+
+import passport from 'passport';
+import passportLocalMongoose from 'passport-local-mongoose';
+import connectEnsureLogin from 'connect-ensure-login';
 
 const app = express();
-
-
 const port = 3000;
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressSession)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 mongoose.connect('mongodb://localhost/AlephChat')
 
-const userSchema =  new mongoose.Schema({
-	username: String,
-	password: String
-})
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
 
+userSchema.plugin(passportLocalMongoose)
 
 const User = mongoose.model('User', userSchema);
+passport.use(User.createStrategy())
 
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
-User.create({ username: 'Jimbob', password:  'password'})
-User.create({ username: 'Micheal Scott', password:  'invalid'})
-User.create({ username: 'Debby', password:  'password23'})
+//User.create({ username: 'Ralph', password: 'password'})
+//User.create({ username: 'Michael Scott', password: 'invalid'})
+//User.create({ username: 'Debby', password: 'password123'})
 
-app.get('/', (req, res) =>{
-    res.send('hello world')
-} )
+//User.register({ username: 'jackson', active:false}, 'password')
 
-app.get('/users/:username', (req, res) => {
-    // look up the user
+app.get('/', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    res.sendFile('views/index.html', { root: __dirname })
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile('views/login.html', { root: __dirname })
+})
+
+app.get('/register', (req,res) => {
+    res.sendFile('views/register.html', { root: __dirname })
+})
+
+app.post('/register', (req,res) =>{
+    const user = User.register({ username: req.body.username, active: false }, req.body.password, (err, user) =>{
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/')
+        })
+    })
+})
+
+app.post('/login', passport.authenticate('local'), (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/users/:username', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
     User.findOne({ username: req.params.username }, (err, user) => {
-    if (err){
-        res.send({ error: err})
-    // do something
-    } else {
-    res.send({ user: user })
-    }
+        if (err){
+            //do something
+            res.send({ error: err })
+        } else {
+            res.send({ user:user })
+        }
+    })
 })
-})
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`)
 })
-
 
 console.log(mongoose)
