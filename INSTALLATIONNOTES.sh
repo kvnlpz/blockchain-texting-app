96d414e0422e12369121f4610dd34cc4c9b5cdca
Kevin L <kevinlopez8554@csu.fullerton.edu>
2021-11-30 17:23:08 -0800

    Update INSTALLATIONNOTES.sh

--- INSTALLATIONNOTES.sh
+++ INSTALLATIONNOTES.sh
@@ -1,10 +1,17 @@
-npm init -y
+sudo apt update && sudo apt upgrade -y
 
+#update npm 
+npm install -g npm@latest
+
+npm init -y
 
 npm install express nodemon
 
+npm install mongoose
 
 npm install babel-cli babel-preset-env babel-preset-stage-0 --save--dev
+
+npm install body-parser express-session passport passport-local-mongoose connect-ensure-login
 
 
 # change "test" in package.json to this:  "start": "nodemon ./index.js ==exec babel-node -e js"
@@ -18,8 +25,25 @@ touch index.js
 
 #NEED TO INSTALL MONGODB
 
-#update npm 
+echo "installing mongoDB"
 
-npm install mongoose
+# following commands are for Linux Mint only
 
-npm install body-parser express-session passport passport-local-mongoose connect-ensure-login
+sudo apt update && sudo apt install gnupg -y
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/
+mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt install mongodb-org
+
+sudo systemctl start mongod
+
+sudo systemctl enable mongod
+
+mongo
+
+# use AlephChat
+
+
