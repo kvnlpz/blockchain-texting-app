4d58690abe0c2b7a4c1759c8cbc8538b3633d728
rpura92509 <rpura951@gmail.com>
2021-12-04 16:10:31 -0800

    -a
    Updated to video 8

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
+const aleph = require('aleph-js');
+
 const expressSession = require('express-session')({
     secret: 'insert secret here',
     resave: false,
@@ -13,7 +15,7 @@ import passportLocalMongoose from 'passport-local-mongoose';
 import connectEnsureLogin from 'connect-ensure-login';
 
 const app = express();
-const port = 3000;
+const port = 4567;
 
 app.use(express.static(__dirname))
 
@@ -24,11 +26,17 @@ app.use(expressSession)
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.set('view engine', 'ejs')
+
 mongoose.connect('mongodb://localhost/AlephChat')
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    private_key: String,
+    public_key: String,
+    mnemonics: String,
+    address: String
 });
 
 userSchema.plugin(passportLocalMongoose)
@@ -46,7 +54,14 @@ passport.deserializeUser(User.deserializeUser())
 //User.register({ username: 'jackson', active:false}, 'password')
 
 app.get('/', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
-    res.sendFile('views/index.html', { root: __dirname })
+    var room = 'hall'
+    var api_server = 'https://api2.aleph.im'
+    var network_id = 261
+    var channel = 'Test'
+    aleph.posts.get_posts('chat', {'refs': [room], 'api_server': api_server}).then((result) =>{
+        res.render('index', {posts: result.posts, user: req.user})
+    })
+    
 })
 
 app.get('/login', (req,res) => {
@@ -57,11 +72,21 @@ app.get('/register', (req,res) => {
     res.sendFile('views/register.html', { root: __dirname })
 })
 
-app.post('/register', (req,res) =>{
-    const user = User.register({ username: req.body.username, active: false }, req.body.password, (err, user) =>{
-        passport.authenticate('local')(req, res, () => {
-            res.redirect('/')
+// User Registration
+app.post('/register', (req,res) => {
+    User.register({ username: req.body.username, active: false }, req.body.password, (err, user) => {
+        aleph.ethereum.new_account().then((eth_account) => {
+            user.private_key = eth_account.private_key
+            user.public_key = eth_account.public_key
+            user.mnemonics = eth_account.mnemonics
+            user.adress = eth_account.address
+            user.save()
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/')
+            })
         })
+
+        
     })
 })
 
@@ -69,10 +94,28 @@ app.post('/login', passport.authenticate('local'), (req, res) => {
     res.redirect('/')
 })
 
+// Pushes messages to Aleph network
+app.post("/messages", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    var message = req.body.message
+    aleph.ethereum.import_account({ mnemonics: req.user.mnemonics}).then((account) => {
+        var room = 'hall'
+        var api_server = 'https://api2.aleph.im'
+        var network_id = 261
+        var channel = 'Test'
+
+        aleph.posts.submit(account.address, 'chat', { 'body' : message }, {
+            ref: room,
+            api_server: api_server,
+            account: account,
+            channel: channel
+        })
+    })
+})
+
 app.get('/users/:username', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    // look up the user
     User.findOne({ username: req.params.username }, (err, user) => {
         if (err){
-            //do something
             res.send({ error: err })
         } else {
             res.send({ user:user })
